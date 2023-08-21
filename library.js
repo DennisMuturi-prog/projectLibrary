8c137042c3250a50627e560608481afb18b2a66f
Dennis Muturi <denismutur@gmail.com>
2023-08-21 23:23:22 +0300

    Add a toggle to enable read or unread

--- library.js
+++ library.js
@@ -3,13 +3,13 @@ const book={
     author:'example',
     title:'example',
     pages:0,
-    read:true
+    status:true
 }
 let book1=Object.create(book);
 book1.author='Dennis';
 book1.title='Kenya';
 book1.pages=190;
-book1.read=true;
+book1.status='read';
 books.push(book1);
 const mydiv=document.querySelector('.books');
 const bookdiv=document.createElement('div');
@@ -19,6 +19,9 @@ for(let book of books){
         const p=document.createElement('p');
         const label=document.createElement('label');  
         const span=document.createElement('span');
+        if(x=='status'){
+            span.setAttribute('class','status');
+        }
         p.appendChild(label);
         p.appendChild(span);
         label.textContent=x+':';
@@ -29,17 +32,31 @@ for(let book of books){
 let i=books.length-1;
 i+='';
 const myButton=document.createElement('button');
+const readCheck=document.createElement('input');
+readCheck.setAttribute('type','checkbox');
+readCheck.checked=true;
 myButton.textContent='remove';
 myButton.setAttribute('data',i);
 bookdiv.appendChild(myButton);
+bookdiv.appendChild(readCheck);
 bookdiv.setAttribute('id',i);
 mydiv.appendChild(bookdiv);
 myButton.addEventListener('click',function(){
     let text=this.getAttribute('data');
-    
     const element=document.getElementById(text);
     element.remove();
-
+})
+readCheck.addEventListener('change',function(){
+    if(book1.status=='read'){
+        book1.status='unread';
+        const span1=document.querySelector('.status');
+        span1.textContent='unread';
+    }
+    else if(book1.status=='unread'){
+        book1.status='read';
+        const span1=document.querySelector('.status');
+        span1.textContent='read';
+    }
 })
 
 const addbutton=document.querySelector('#addbook');
@@ -58,13 +75,25 @@ function addBook(){
     book2.author=authorinput.value;
     book2.title=titleinput.value;
     book2.pages=pagesinput.value;
-    book2.read=readinput.value;
+    if(readinput.checked){
+        book2.status='read'
+    }
+    else{
+        book2.status='unread'
+    }
     books.push(book2);
     const newbook=document.createElement('div');
     for(let x in book2){
         const newp=document.createElement('p');
         const newlabel=document.createElement('label');  
         const newspan=document.createElement('span');
+        let k=books.length-1;
+        k+='';
+        let l='_';
+        l+=k;
+        if(x=='status'){
+            newspan.setAttribute('class',l);
+        }
         newp.appendChild(newlabel);
         newp.appendChild(newspan);
         newlabel.textContent=x+':';
@@ -74,10 +103,16 @@ function addBook(){
     let j=books.length-1;
     j+='';
     const newbutton=document.createElement('button');
+    const newreadcheck=document.createElement('input');
+    newreadcheck.setAttribute('type','checkbox');
+    if(book2.status=='read'){
+        newreadcheck.checked=true;
+    }
     newbutton.setAttribute('data',j);
     newbutton.textContent='remove';
     newbook.setAttribute('id',j);
     newbook.appendChild(newbutton);
+    newbook.appendChild(newreadcheck);
     mydiv.appendChild(newbook);
     newbutton.addEventListener('click',function(){
         const confirmation=confirm('are you sure?');
@@ -87,6 +122,20 @@ function addBook(){
             element.remove();
         }
        
+    })
+    let y='_';
+    y+=j;
+    const span3=document.querySelector('.'+y);
+    newreadcheck.addEventListener('change',function(){
+        console.log(book2.status);
+        if(book2.status=='read'){
+            book2.status='unread';
+            span3.textContent='unread';
+        }
+        else if(book2.status=='unread'){
+            book2.status='read';
+            span3.textContent='read';
+        }
     })
 
 }
