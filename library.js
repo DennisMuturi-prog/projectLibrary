63aa9bf26693a47cd682e59d0ea11f4c580cce91
Dennis Muturi <denismutur@gmail.com>
2023-08-22 13:37:13 +0300

    Optimise code by adding functions for creating the book cards

--- library.js
+++ library.js
@@ -11,66 +11,68 @@ book0.author='Reuben';
 book0.title='Ps5andPC';
 book0.pages=150;
 book0.status='unread';
-books.push(book0);
 book1.author='Dennis';
 book1.title='Kenya';
 book1.pages=190;
 book1.status='read';
-books.push(book1);
 const mydiv=document.querySelector('.books');
-let booksLen=0;
-for(let book of books){
-    const bookdiv=document.createElement('div');
-    for(let x in book){
-        const p=document.createElement('p');
-        const label=document.createElement('label');  
-        const span=document.createElement('span');
-        p.appendChild(label);
-        p.appendChild(span);
-        label.textContent=x+':';
-        span.textContent=book[x]; 
-        bookdiv.appendChild(p);
+function createBook(librarybook){
+        const bookdiv=document.createElement('div');
+        for(let x in librarybook){
+            const p=document.createElement('p');
+            const label=document.createElement('label');  
+            const span=document.createElement('span');
+            p.appendChild(label);
+            p.appendChild(span);
+            label.textContent=x+':';
+            span.textContent=librarybook[x]; 
+            bookdiv.appendChild(p);
+        }
+        let i='_'+(books.length-1);
+        console.log(books.length);
+    const removeButton=document.createElement('button');
+    const readCheck=document.createElement('input');
+    readCheck.setAttribute('type','checkbox');
+    if(librarybook.status=='read'){
+        readCheck.checked=true;
     }
-    let i='_'+booksLen;
-    booksLen++;
-    console.log(books.length);
-const removeButton=document.createElement('button');
-const readCheck=document.createElement('input');
-readCheck.setAttribute('type','checkbox');
-if(book.status=='read'){
-    readCheck.checked=true;
+    else{
+        readCheck.checked=false;
+    }
+    
+    removeButton.textContent='remove';
+    removeButton.setAttribute('data',i);
+    bookdiv.appendChild(removeButton);
+    bookdiv.appendChild(readCheck);
+    bookdiv.setAttribute('id',i);
+    mydiv.appendChild(bookdiv);
+    removeButton.addEventListener('click',function(){
+        const confirmation=confirm('are you sure?');
+        if(confirmation){
+            let text=this.getAttribute('data');
+            const element=document.getElementById(text);
+            element.remove();
+        }
+       
+    })
+    const span1=document.querySelector('#'+i+'>p:nth-child(4)>span');
+    readCheck.addEventListener('change',function(){
+        if(librarybook.status=='read'){
+            librarybook.status='unread'; 
+            span1.textContent='unread';
+        }
+        else if(librarybook.status=='unread'){
+            librarybook.status='read';
+            span1.textContent='read';
+        }
+    })
+    
 }
-else{
-    readCheck.checked=false;
-}
+ books.push(book0);
+ createBook(book0);
+ books.push(book1);
+ createBook(book1);
 
-removeButton.textContent='remove';
-removeButton.setAttribute('data',i);
-bookdiv.appendChild(removeButton);
-bookdiv.appendChild(readCheck);
-bookdiv.setAttribute('id',i);
-mydiv.appendChild(bookdiv);
-removeButton.addEventListener('click',function(){
-    const confirmation=confirm('are you sure?');
-    if(confirmation){
-        let text=this.getAttribute('data');
-        const element=document.getElementById(text);
-        element.remove();
-    }
-   
-})
-const span1=document.querySelector('#'+i+'>p:nth-child(4)>span');
-readCheck.addEventListener('change',function(){
-    if(book.status=='read'){
-        book.status='unread'; 
-        span1.textContent='unread';
-    }
-    else if(book.status=='unread'){
-        book.status='read';
-        span1.textContent='read';
-    }
-})
-}
 const addbutton=document.querySelector('#addbook');
 const mydialog=document.querySelector('dialog');
 addbutton.addEventListener('click',function(){
@@ -94,60 +96,10 @@ function addBook(){
         book2.status='unread'
     }
     books.push(book2);
-    const newbook=document.createElement('div');
-    for(let x in book2){
-        const newp=document.createElement('p');
-        const newlabel=document.createElement('label');  
-        const newspan=document.createElement('span');
-        newp.appendChild(newlabel);
-        newp.appendChild(newspan);
-        newlabel.textContent=x+':';
-        newspan.textContent=book2[x]; 
-        newbook.appendChild(newp);
-    }
-    let j='_'+(books.length-1);
-    const newbutton=document.createElement('button');
-    const newreadcheck=document.createElement('input');
-    newreadcheck.setAttribute('type','checkbox');
-    if(book2.status=='read'){
-        newreadcheck.checked=true;
-    }
-    newbutton.setAttribute('data',j);
-    newbutton.textContent='remove';
-    newbook.setAttribute('id',j);
-    newbook.appendChild(newbutton);
-    newbook.appendChild(newreadcheck);
-    mydiv.appendChild(newbook);
-    newbutton.addEventListener('click',function(){
-        const confirmation=confirm('are you sure?');
-        if(confirmation){
-            let text=this.getAttribute('data');
-            const element=document.getElementById(text);
-            element.remove();
-        }
-       
-    })
-    const span3=document.querySelector('#'+j+'>p:nth-child(4)>span');
-    newreadcheck.addEventListener('change',function(){
-        if(book2.status=='read'){
-            book2.status='unread';
-            span3.textContent='unread';
-        }
-        else if(book2.status=='unread'){
-            book2.status='read';
-            span3.textContent='read';
-        }
-    })
-
+    createBook(book2);
 }
 confirmBtn.addEventListener('click',function(){
     addBook();
     mydialog.close();
     form.reset();
 });
-
-
-
-
-
-
